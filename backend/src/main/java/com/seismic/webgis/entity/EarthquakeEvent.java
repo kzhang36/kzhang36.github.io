2ea40fe6003b5b6a9993ce95c66b1408452d17f9
Snow_Z <kzhang36@buffalo.edu>
2025-07-28 15:55:04 +0800

    Update EarthquakeEvent.java

--- backend/src/main/java/com/seismic/webgis/entity/EarthquakeEvent.java
+++ backend/src/main/java/com/seismic/webgis/entity/EarthquakeEvent.java
@@ -1,5 +1,6 @@
 package com.seismic.webgis.entity;
 
+import jakarta.persistence.*;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
