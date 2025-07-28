2b2cc2429b410f00df2515c64789d2ba39ca0e49
Snow_Z <kzhang36@buffalo.edu>
2025-07-28 15:59:52 +0800

    Update schema.sql

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -7,3 +7,4 @@ CREATE TABLE IF NOT EXISTS earthquakes (
    depth DOUBLE PRECISION,
    coordinates GEOMETRY(Point, 4326)
  );
+ CREATE INDEX IF NOT EXISTS idx_geom ON earthquakes USING GIST (geom);
