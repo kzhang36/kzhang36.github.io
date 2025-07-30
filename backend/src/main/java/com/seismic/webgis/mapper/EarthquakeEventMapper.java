72eb11faf25902df1b2084928be14a7cefdbcf48
Snow_Z <kzhang36@buffalo.edu>
2025-07-30 17:05:13 +0800

    Delete backend directory

--- backend/src/main/java/com/seismic/webgis/mapper/EarthquakeEventMapper.java
+++
@@ -1,9 +0,0 @@
-package com.seismic.webgis.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.seismic.webgis.entity.EarthquakeEvent;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface EarthquakeEventMapper extends BaseMapper<EarthquakeEvent> {
-}
--- backend/src/main/java/com/seismic/webgis/repository/EarthquakeRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.seismicgis.repository;
-import com.example.seismicgis.entity.Earthquake;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface EarthquakeRepository extends JpaRepository<Earthquake, String> {}
